e4b9d72ee799a813b423809ba8b672ba07494f0b
Alexandra <caapricorn@mail.ru>
2021-10-22 21:33:11 +0300

    Scripted auto-commit on change (2021-10-22 21:33:11) by gitwatch.sh

--- src/main/java/AirportWritable.java
+++ src/main/java/AirportWritable.java
@@ -1,2 +1,11 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class AirportWritable {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("L_AIRPORT.csv"));
+        
+    }
 }
