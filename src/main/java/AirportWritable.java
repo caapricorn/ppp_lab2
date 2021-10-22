4c78052f14c27c9d335fbf6ad6c30e5d49814724
Alexandra <caapricorn@mail.ru>
2021-10-22 21:40:41 +0300

    Scripted auto-commit on change (2021-10-22 21:40:41) by gitwatch.sh

--- src/main/java/AirportWritable.java
+++ src/main/java/AirportWritable.java
@@ -1,11 +1,22 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class AirportWritable {
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader("L_AIRPORT.csv"));
-        
+        String line = null;
+        Scanner scanner = null;
+        int index = 0;
+        List<Airport> airportList = new ArrayList<>();
+        while ((line = reader.readLine()) != null) {
+            Airport airport = new Airport();
+            scanner = new Scanner(line);
+            scanner.useDelimiter()
+        }
     }
 }
