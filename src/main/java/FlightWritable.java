2a955c64fe6cfd5cd3fee1bd011989cca8f1b42e
Alexandra <caapricorn@mail.ru>
2021-10-22 23:27:00 +0300

    Scripted auto-commit on change (2021-10-22 23:27:00) by gitwatch.sh

--- src/main/java/FlightWritable.java
+++ src/main/java/FlightWritable.java
@@ -3,6 +3,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class FlightWritable {
@@ -20,9 +21,9 @@ public class FlightWritable {
             scanner.useDelimiter(",");
             while (scanner.hasNext()) {
                 String data = scanner.next();
-                if (index == 14 && data != null) flight.setDEST_AEROPORT_ID(Integer.parseInt(data));
-                else if (index == 17 && data != null) flight.setARR_DELAY(Double.parseDouble(data));
-                else if (index == 18 && data != null) flight.setARR_DELAY_NEW(Double.parseDouble(data));
+                if (index == 14 && !Objects.equals(data, "")) flight.setDEST_AEROPORT_ID(Integer.parseInt(data));
+                else if (index == 17 && !Objects.equals(data, "")) flight.setARR_DELAY(Double.parseDouble(data));
+                else if (index == 18 && !Objects.equals(data, "")) flight.setARR_DELAY_NEW(Double.parseDouble(data));
                 //else System.out.println("Некорректные данные::" + data);
                 index++;
             }
