5b39ae779e4dd2ed68f30f34c4639a435a127e8d
Alexandra <caapricorn@mail.ru>
2021-11-11 23:07:12 +0300

    Scripted auto-commit on change (2021-11-11 23:07:12) by gitwatch.sh

--- src/main/java/FlightMapper.java
+++ src/main/java/FlightMapper.java
@@ -4,7 +4,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
-public class FlightMapper {
+public class FlightMapper extends Mapper{
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException,
             InterruptedException {
