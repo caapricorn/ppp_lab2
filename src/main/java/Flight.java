2767fccccbd4e52be1d77608861059032b484789
Alexandra <caapricorn@mail.ru>
2021-11-11 22:37:52 +0300

    Scripted auto-commit on change (2021-11-11 22:37:52) by gitwatch.sh

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -13,25 +13,25 @@ public class Flight {
         return DEST_AEROPORT_ID;
     }
 
-    public void setDEST_AEROPORT_ID(int DEST_AEROPORT_ID) {
-        this.DEST_AEROPORT_ID = DEST_AEROPORT_ID;
-    }
+    //public void setDEST_AEROPORT_ID(int DEST_AEROPORT_ID) {
+    //    this.DEST_AEROPORT_ID = DEST_AEROPORT_ID;
+    //}
 
     public double getARR_DELAY() {
         return ARR_DELAY;
     }
 
-    public void setARR_DELAY(double ARR_DELAY) {
-        this.ARR_DELAY = ARR_DELAY;
-    }
+    //public void setARR_DELAY(double ARR_DELAY) {
+    //    this.ARR_DELAY = ARR_DELAY;
+    //}
 
     public double getARR_DELAY_NEW() {
         return ARR_DELAY_NEW;
     }
 
-    public void setARR_DELAY_NEW(double ARR_DELAY_NEW) {
-        this.ARR_DELAY_NEW = ARR_DELAY_NEW;
-    }
+    //public void setARR_DELAY_NEW(double ARR_DELAY_NEW) {
+    //    this.ARR_DELAY_NEW = ARR_DELAY_NEW;
+    //}
 
     public static Flight parse(String csv) {
         String[] list = new String[3];
