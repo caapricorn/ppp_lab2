439713bd9f51ccbe2def87e477b2e1909eca7d9f
Alexandra <caapricorn@mail.ru>
2021-11-11 21:18:42 +0300

    Scripted auto-commit on change (2021-11-11 21:18:42) by gitwatch.sh

--- src/main/java/CountApp.java
+++ src/main/java/CountApp.java
@@ -14,12 +14,13 @@ public class CountApp {
         Job job = Job.getInstance();
         job.setJarByClass(CountApp.class);
         job.setJobName("Count App");
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        //FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
         //job.setMapperClass(WordMapper.class);
         //job.setReducerClass(WordReducer.class);
         //job.setOutputKeyClass(Text.class);
         //job.setOutputValueClass(IntWritable.class);
         //job.setNumReduceTasks(2);
         System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
 }
