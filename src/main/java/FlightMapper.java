938eca9b2c1edfe73e80dcc7e55be7dde00167d6
Alexandra <caapricorn@mail.ru>
2021-11-11 23:08:15 +0300

    Scripted auto-commit on change (2021-11-11 23:08:15) by gitwatch.sh

--- src/main/java/FlightMapper.java
+++ src/main/java/FlightMapper.java
@@ -9,9 +9,10 @@ public class FlightMapper extends Mapper<LongWritable, Text, Text, IntWritable>{
     protected void map(LongWritable key, Text value, Context context) throws IOException,
             InterruptedException {
         String line = value.toString();
-        String[] words = line.split("[\\p{Punct}\\p{Space}—]");
-        for (String word : words) {
-            context.write(new Text(word), new IntWritable(1));
-        }
+        Flight flight = Flight.parse(line);
+        //String[] words = line.split("[\\p{Punct}\\p{Space}—]");
+//        for (String word : words) {
+//            context.write(new Text(word), new IntWritable(1));
+//        }
     }
 }
