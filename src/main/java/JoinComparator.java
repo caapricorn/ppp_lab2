b05b8a4eb09510cbca5a3f2c9510f56004d196b3
Alexandra <caapricorn@mail.ru>
2021-11-12 00:19:58 +0300

    Scripted auto-commit on change (2021-11-12 00:19:58) by gitwatch.sh

--- src/main/java/JoinComparator.java
+++ src/main/java/JoinComparator.java
@@ -2,5 +2,7 @@ import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
 public class JoinComparator extends WritableComparator {
-    
+    public JoinComparator() {
+        super();
+    }
 }
