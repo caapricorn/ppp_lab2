de5c2d3897b02e529a98c58aae4e96bd05ef8db5
Alexandra <caapricorn@mail.ru>
2021-11-12 00:30:35 +0300

    Scripted auto-commit on change (2021-11-12 00:30:35) by gitwatch.sh

--- src/main/java/JoinReducer.java
+++ src/main/java/JoinReducer.java
@@ -4,5 +4,15 @@ import java.io.IOException;
 import java.util.Iterator;
 
 public class JoinReducer extends Reducer<JoinWritableComparable, Text, Text, Text{
-    
+    @Override
+    protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws
+            IOException, InterruptedException {
+        long count=0;
+        Iterator iter = values.iterator();
+        while(iter.hasNext()) {
+            iter.next();
+            count++;
+        }
+        context.write(key, new LongWritable(count));
+    }
 }
