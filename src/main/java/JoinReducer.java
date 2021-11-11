ed561aae7a3984f2a42ef640b814ace6575f67fe
Alexandra <caapricorn@mail.ru>
2021-11-12 00:30:09 +0300

    Scripted auto-commit on change (2021-11-12 00:30:09) by gitwatch.sh

--- src/main/java/JoinReducer.java
+++ src/main/java/JoinReducer.java
@@ -1,2 +1,7 @@
-public class JoinReducer {
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+import java.io.IOException;
+import java.util.Iterator;
+
+public class JoinReducer extends Reducer<JoinWritableComparable, Text,{
 }
