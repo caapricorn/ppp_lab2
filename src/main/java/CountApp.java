f9214ea096b29674993966dbc680031bb806f32b
Alexandra <caapricorn@mail.ru>
2021-11-12 00:52:36 +0300

    Scripted auto-commit on change (2021-11-12 00:52:36) by gitwatch.sh

--- src/main/java/CountApp.java
+++ src/main/java/CountApp.java
@@ -20,11 +20,18 @@ public class CountApp {
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, AirportMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, FlightMapper.class);
 
+        job.setGroupingComparatorClass(JoinComparator.class);
+        job.setPartitionerClass(AirportPartitioner.class);
+        job.setReducerClass(JoinReducer.class);
+
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
         job.setReducerClass(JoinReducer.class);
-        //job.setOutputKeyClass(Text.class);
-        //job.setOutputValueClass(IntWritable.class);
+        
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+
         job.setNumReduceTasks(2);
+
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
