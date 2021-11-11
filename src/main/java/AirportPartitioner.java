b55439f499d8cba5439968430405c46bbc4c961e
Alexandra <caapricorn@mail.ru>
2021-11-12 00:27:54 +0300

    Scripted auto-commit on change (2021-11-12 00:27:54) by gitwatch.sh

--- src/main/java/AirportPartitioner.java
+++ src/main/java/AirportPartitioner.java
@@ -4,6 +4,6 @@ import org.apache.hadoop.mapreduce.Partitioner;
 public class AirportPartitioner extends Partitioner<JoinWritableComparable, Text> {
     @Override
     public int getPartition(JoinWritableComparable key, Text value, int i) {
-        
+        return (key.getAirportId() & Integer.MAX_VALUE) % i;
     }
 }
