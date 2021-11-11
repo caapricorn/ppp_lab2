bd7bf6f6a10103c4e05527b33b655a1c3d9a2b0f
Alexandra <caapricorn@mail.ru>
2021-11-11 21:44:26 +0300

    Scripted auto-commit on change (2021-11-11 21:44:26) by gitwatch.sh

--- src/main/java/AirportMapper.java
+++ src/main/java/AirportMapper.java
@@ -5,5 +5,13 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class AirportMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
-    
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException,
+            InterruptedException {
+        String line = value.toString();
+        String[] words = line.split("[\\p{Punct}\\p{Space}—]");
+        for (String word : words) {
+            context.write(new Text(word), new IntWritable(1));
+        }
+    }
 }
