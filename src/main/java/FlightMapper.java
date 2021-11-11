25f331d45c421d9a722d91e229ad1e65857932b7
Alexandra <caapricorn@mail.ru>
2021-11-11 23:06:27 +0300

    Scripted auto-commit on change (2021-11-11 23:06:27) by gitwatch.sh

--- src/main/java/FlightMapper.java
+++ src/main/java/FlightMapper.java
@@ -1,2 +1,8 @@
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import java.io.IOException;
+
 public class FlightMapper {
 }
