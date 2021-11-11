50d647d7959a73e3267081eaa6dba33cbefdd717
Alexandra <caapricorn@mail.ru>
2021-11-11 21:44:00 +0300

    Scripted auto-commit on change (2021-11-11 21:44:00) by gitwatch.sh

--- src/main/java/AirportMapper.java
+++ src/main/java/AirportMapper.java
@@ -4,5 +4,5 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
-public class AirportMapper {
+public class AirportMapper extends Mapper<> {
 }
