25cbfb8b40f8376d3509d86ab571ad46f153b5d5
Alexandra <caapricorn@mail.ru>
2021-11-12 00:12:17 +0300

    Scripted auto-commit on change (2021-11-12 00:12:17) by gitwatch.sh

--- src/main/java/FlightMapper.java
+++ src/main/java/FlightMapper.java
@@ -4,18 +4,19 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
-public class FlightMapper extends Mapper<LongWritable, Text, Text, IntWritable>{
+public class FlightMapper extends Mapper<LongWritable, Text, JoinWritableComparable, IntWritable>{
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException,
             InterruptedException {
         if (key.get() > 0) {
             String line = value.toString();
             Flight flight = Flight.parse(line);
-            //String[] words = line.split("[\\p{Punct}\\p{Space}—]");
-            context.write(
-                    new Text(word),
-                    new IntWritable(1)
-            );
+            if (flight.getARR_DELAY() > 0) {
+                context.write(
+                        new JoinWritableComparable(flight.getDEST_AEROPORT_ID(), true),
+                        new IntWri
+                );
+            }
         }
     }
 }
